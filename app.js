38eeb293bbf11d6fb339f5aaf3087c06173cfca8
ZakiZughbi <zakizughbi@gmail.com>
2021-03-13 18:36:16 -0500

    completed website -level 2 animations

---
+++ app.js
@@ -0,0 +1,58 @@
+
+const menuBars = document.querySelector('.menuBars');
+const navLinks = document.querySelector('.navlinks');
+const translateBtn = document.querySelector('.translateContainer img');
+const lan = document.querySelector('.lanContainer');
+const arabic = document.querySelector('.lan').children[0];
+const spainsh = document.querySelector('.lan').children[1];
+const english = document.querySelector('.lan').children[2];
+const homeHeader = document.querySelector('.homeImg1 h2')
+let clicked = 0;
+let lanClicked = 0;
+const headerList = ['Creativity', 'Simplicity', 'Modernity'];
+
+
+
+menuBars.addEventListener('click', ()=>{
+    clicked ++
+    if (clicked == 1){
+        menuBars.children[0].style.animation="bar1In 0.5s forwards";
+        menuBars.children[1].style.animation="bar2In 0.5s forwards";
+        menuBars.children[2].style.width="0";
+        navLinks.style.width="100%"
+    } else{
+        clicked = 0;
+        menuBars.children[0].style.animation="bar1out 0.5s forwards";
+        menuBars.children[1].style.animation="bar2out 0.5s forwards";
+        menuBars.children[2].style.width="30px";
+        navLinks.style.width="0%"
+    }
+});
+
+translateBtn.addEventListener('click', ()=>{
+    lanClicked ++
+    if (lanClicked == 1){
+        //lanClicked = 0;
+        lan.style.display="block";
+    } else{
+        lanClicked = 0;
+        lan.style.display="none";
+    }
+
+
+});
+
+
+// setTimeout(()=>{
+//     for(let i=0; i<headerList.length; i++){
+//         homeHeader.innerHTML=''
+//         homeHeader.innerHTML=headerList[i]
+//     }
+// }, 5000)
+
+for(let i=0; i<headerList.length; i++){
+    setTimeout(()=>{
+        homeHeader.innerHTML='';
+        homeHeader.innerHTML=headerList[i];
+    }, 3000)
+}
